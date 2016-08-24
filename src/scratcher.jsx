27e58913a4636cc4d10175ca28f3e22f53aa80d7
René Monroy <rene.monroy@gmail.com>
2016-08-24 13:47:06 -0500

    Create a pure function for the component to act as an initial test

---
+++ src/scratcher.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Scratcher = function Scratcher() {
+  return (
+    <div className="scratcher">
+      <p>Scratcher</p>
+    </div>
+  );
+};
+
+export default Scratcher;
